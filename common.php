c793c9525f2fc86ee52105cd484a7c65ab0e3b12
Brenton Jackson <bjavonjackson@gmail.com>
2022-07-08 12:24:33 -0400

    feat: add images, favicon, and flesh out navbar

--- common.php
+++ common.php
@@ -5,15 +5,16 @@
 function head() {
     echo "<!DOCTYPE html><html><head><meta charset=\"utf-8\" />";
     echo "<title>Hang Man</title>";
+    echo "<link rel=\"icon\" type=\"image/x-icon\" href=\"./images/favicon/favicon.ico\">";
     echo "<link href=\"./hangman.css\" rel=\"stylesheet\"/></head>";
     echo "<body>";
 }
 
 function navbar() {
     echo "<div class=\"navbar\">";
-    echo "<div class=\"navbar-item\">Logo Image/homepage link</div>";
-    echo "<div class=\"navbar-item\">Leaderboard button</div>";
-    echo "<div class=\"navbar-item\">Username/signin/out stuff</div>";
+    echo "<div class=\"navbar-item home-logo\"><a href=\"./index.php\"><img src=\"./images/favicon/apple-touch-icon.png\" height=\"50px\"></a></div>";
+    echo "<div class=\"navbar-item leaderboard\"><a href=\"./leaderboard.php\">View Leaderboard</a></div>";
+    echo "<div class=\"navbar-item sign-in\">Username/signin/out stuff</div>";
     echo "</div>";
 }
 
